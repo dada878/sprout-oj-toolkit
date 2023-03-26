a40dbc9ce5ba9d256ac38d697381395554907d70
Dada878 <xx0932399@gmail.com>
2023-03-26 11:47:53 +0800

    優化程式碼

--- scripts/main.js
+++ scripts/main.js
@@ -16,49 +16,78 @@ class AnswerDB {
     }
 }
 
+class QueryHelper {
+    static queryProblems() {
+        return document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
+    }
+    static queryDescriptionOfProblem(problem) {
+        return problem.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
+    }
+    static queryOptionsOfProblem(problem) {
+        return problem.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
+    }
+    static queryRightOptionsOfProblem(problem) {
+        return problem.querySelectorAll('.rightOption');
+    }
+    static queryProblemSwitchButtons() {
+        return document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])');
+    }
+    static queryNavbar() {
+        return document.getElementsByClassName("hidden-xs ng-scope")[0];
+    }
+    static queryLoadAnswerButton() {
+        return document.getElementById("chwa-helper-answer-btn");
+    }
+    static queryActionButtonGroup() {
+        return document.querySelector(`div[ng-hide=showTotalScore]`);
+    }
+}
+
 class PadeRender {
+    static createElementFromHTML(htmlString) {
+        const ele = document.createElement("div");
+        ele.innerHTML = htmlString;
+        return ele.firstChild;
+    }
+    static isFinishOrQuizPage() {
+        return !!QueryHelper.queryActionButtonGroup();
+    }
     static render() {
         if (!this.isRendered()) {
-            if (!document.querySelector(`div[ng-hide=showTotalScore]`)) return;
+            if (!this.isFinishOrQuizPage()) return;
             this.injectAds();
             this.addLoadAnswerBtn();
             this.addGuessBtn();
         }
-        document.getElementById("chwa-helper-answer-btn").style.cursor = AnswerDB.empty() ? "not-allowed" : "pointer";
+        QueryHelper.queryLoadAnswerButton().style.cursor = AnswerDB.empty() ? "not-allowed" : "pointer";
     }
     static isRendered() {
-        return !!document.getElementById("chwa-helper-answer-btn");
+        return !!QueryHelper.queryLoadAnswerButton();
     }
     static injectAds() {
-        const navbar = document.getElementsByClassName("hidden-xs ng-scope")[0];
-        const ele = document.createElement("div");
-        ele.innerHTML = `<div class="col-xs-3 col-md-3" src="images/ChwaLogo3.png" style=""><h3 style="font-weight: bolder;color: #003B89;filter: drop-shadow(0px 0px 3px lightblue);cursor: pointer;" onclick="window.open('https://microsoftedge.microsoft.com/addons/detail/%E5%85%A8%E8%8F%AF%E5%8A%A9%E6%89%8B/caddmfhjodlapcapohemggjjaboahpdp','_blank')">全華助手 v1.0.2</h3></div>`;
-        navbar.appendChild(ele.firstChild);
+        QueryHelper.queryNavbar().appendChild(
+            this.createElementFromHTML(`<div class="col-xs-3 col-md-3" src="images/ChwaLogo3.png" style=""><h3 style="font-weight: bolder;color: #003B89;filter: drop-shadow(0px 0px 3px lightblue);cursor: pointer;" onclick="window.open('https://microsoftedge.microsoft.com/addons/detail/%E5%85%A8%E8%8F%AF%E5%8A%A9%E6%89%8B/caddmfhjodlapcapohemggjjaboahpdp','_blank')">全華助手 v1.0.2</h3></div>`)
+        );
     }
     static addLoadAnswerBtn() {
-        const loadAnswerBtn = document.getElementById("chwa-helper-answer-btn");
-        if (loadAnswerBtn) return;
-        const btnElement = document.createElement('div');
-        btnElement.innerHTML = `<button id="chwa-helper-answer-btn" class="btn" ng-hide="showQ==Qcount" ng-click="nextQ()" onclick="const evt = document.createEvent('Event');evt.initEvent('loadAnswer', true, false);document.dispatchEvent(evt);" style="background-color: rgb(139 28 255); color: rgb(255, 255, 255); padding: 6px 12px; font-weight: bolder;">填入答案</button>`;
-        document.querySelector(`div[ng-hide=showTotalScore]`).appendChild(btnElement.firstChild);
+        QueryHelper.queryActionButtonGroup().appendChild(
+            this.createElementFromHTML(`<button id="chwa-helper-answer-btn" class="btn" ng-hide="showQ==Qcount" ng-click="nextQ()" onclick="const evt = document.createEvent('Event');evt.initEvent('loadAnswer', true, false);document.dispatchEvent(evt);" style="background-color: rgb(139 28 255); color: rgb(255, 255, 255); padding: 6px 12px; font-weight: bolder;">填入答案</button>`)
+        );
     }
     static addGuessBtn() {
-        const loadAnswerBtn = document.getElementById("chwa-helper-guess-btn");
-        if (loadAnswerBtn) return;
-        const btnElement = document.createElement('div');
-        btnElement.innerHTML = `<button id="chwa-helper-guess-btn" class="btn" ng-hide="showQ==Qcount" ng-click="nextQ()" onclick="const evt = document.createEvent('Event');evt.initEvent('guessAnswer', true, false);document.dispatchEvent(evt);" style="background-color: rgb(221 0 0);margin-left: 3px; color: rgb(255, 255, 255); padding: 6px 12px; font-weight: bolder;">猜題</button>`;
-        document.querySelector(`div[ng-hide=showTotalScore]`).appendChild(btnElement.firstChild);
+        QueryHelper.queryActionButtonGroup().appendChild(
+            this.createElementFromHTML(`<button id="chwa-helper-guess-btn" class="btn" ng-hide="showQ==Qcount" ng-click="nextQ()" onclick="const evt = document.createEvent('Event');evt.initEvent('guessAnswer', true, false);document.dispatchEvent(evt);" style="background-color: rgb(221 0 0);margin-left: 3px; color: rgb(255, 255, 255); padding: 6px 12px; font-weight: bolder;">猜題</button>`)
+        );
     }
 }
 
 class QuizManager {
     static saveAnswer() {
         const answer = {};
-        const btns = document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])');
-        const problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
-        for (const pronlem of problems) {
-            const description = pronlem.querySelector('p[ng-bind-html="q.q | trustedHtml"]').innerHTML;
-            const answerOptionElements = pronlem.querySelectorAll('.rightOption');
+        const problems = QueryHelper.queryProblems();
+        for (const problem of problems) {
+            const description = QueryHelper.queryDescriptionOfProblem(problem).innerHTML;
+            const answerOptionElements = QueryHelper.queryRightOptionsOfProblem(problem);
             answer[description] = [];
             for (let opts of answerOptionElements) {
                 answer[description].push(opts.innerHTML);
@@ -70,8 +99,8 @@ class QuizManager {
     static loadAnswer() {
         if (AnswerDB.empty()) return alert("暫無儲存的答案，請先至少交卷一次後再使用");
         const answer = AnswerDB.load();
-        const problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
-        const btns = document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])');
+        const problems = QueryHelper.queryProblems();
+        const btns = QueryHelper.queryProblemSwitchButtons();
         let wrongCount = 0;
         const problemsWriten = [];
         const failedWriten = [];
@@ -88,8 +117,8 @@ class QuizManager {
             return dp[dp.length - 1][dp[0].length - 1];
         }
         const writeAnswer = function (problem, defaultDescription = null) {
-            const description = defaultDescription ?? problem.querySelector('p[ng-bind-html="q.q | trustedHtml"]').innerHTML;
-            const options = problem.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
+            const description = defaultDescription ?? QueryHelper.queryDescriptionOfProblem(problem).innerHTML;
+            const options = QueryHelper.queryOptionsOfProblem(problem);
             let flag = false;
             for (let opt of options) {
                 try {
@@ -131,7 +160,7 @@ class QuizManager {
             if (!problemsWriten.includes(key)) leftAnswerTable[key] = answer[key];
         }
         for (const problem of failedWriten) {
-            const description = problem.querySelector('p[ng-bind-html="q.q | trustedHtml"]').innerHTML;
+            const description = QueryHelper.queryDescriptionOfProblem(problem).innerHTML;
             let minDistance = Number.MAX_SAFE_INTEGER;
             let minDistanceDescription = null;
             for (const answerDescription of Object.keys(leftAnswerTable)) {
@@ -143,17 +172,18 @@ class QuizManager {
             }
             writeAnswer(problem, minDistanceDescription);
         }
-        if (wrongCount) alert(`填入完畢 共有 ${wrongCount} 題作答可能錯誤！`)
+        if (wrongCount) alert(`填入完畢 共有 ${wrongCount} 題作答可能錯誤！`);
+        else alert(`答案填入完畢！`);
         for (const btn of btns) btn.click();
     }
     static guessAnswer() {
-        const problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
+        const problems = QueryHelper.queryProblems();
         for (let problem of problems) {
-            const description = problem.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
-            const options = problem.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
+            const options = QueryHelper.queryOptionsOfProblem(problem);
             options[Math.floor(Math.random() * (3 - 0 + 1)) + 0].click();
         }
-        for (const btn of document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])')) btn.click();
+        for (const btn of QueryHelper.queryProblemSwitchButtons()) btn.click();
+        alert(`猜完所有題目了！`);
     }
 }
 
